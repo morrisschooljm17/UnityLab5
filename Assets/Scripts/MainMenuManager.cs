2670c877adcb4c516c2206718489be681011a382
Jeffrey Miller <Mill7707@umn.edu>
2022-02-21 13:09:54 -0600

    Added Main Menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -11,6 +11,7 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Button openCredits;
     [SerializeField] private Button closeCredits;
     [SerializeField] private Button threeDSpawnButton;
+    [SerializeField] private Button pongButton;
     [SerializeField] private GameObject creditsUI;
     // Start is called before the first frame update
     void Awake()
@@ -20,6 +21,7 @@ public class MainMenuManager : MonoBehaviour
         startGameButton.onClick.AddListener(() => LoadingScreen.LoadScene("SampleScene"));
         notQuitePlatformer.onClick.AddListener(() => LoadingScreen.LoadScene("NotQuitePlatformer"));
         threeDSpawnButton.onClick.AddListener(() => LoadingScreen.LoadScene("3DSpawn"));
+        pongButton.onClick.AddListener(() => LoadingScreen.LoadScene("Pong"));
 
     }
 
